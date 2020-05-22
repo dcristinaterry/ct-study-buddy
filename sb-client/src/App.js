e644bf66529227afa02eb1747369683fc92018ef
CrisTerry <crisdc88@gmail.com>
2020-05-22 12:21:35 -0700

    Add authenticated Admin and user.  Add routes for admin and user

--- sb-client/src/App.js
+++ sb-client/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import './App.css';
-import LoginUser from "./pages/LoginUser"
+import LoginUser from "./pages/userPages/LoginUser"
+
 
 function App() {
   return (
-    <div className="App">
-      <LoginUser/>
-    </div>
+
+    <Router>
+      <div className="App">
+
+       
+
+        <Switch>
+        <Route exact path="/"  component={LoginUser}/>
+
+
+        </Switch>
+
+
+
+      </div>
+
+
+    </Router>
+
   );
 }
 
