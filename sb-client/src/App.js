2d32e7d5519fa274e49a7745fd027976fd6a46f5
guachochi <davis.bradleyj@gmail.com>
2020-05-22 12:41:30 -0700

    merging with master

--- sb-client/src/App.js
+++ sb-client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 // import './App.css';
-import LoginUser from "./pages/LoginUser"
+import LoginUser from "./pages/userPages/LoginUser"
+import { Router } from express
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <LoginUser/>
     </div>
+    </Router>
   );
 }
 
