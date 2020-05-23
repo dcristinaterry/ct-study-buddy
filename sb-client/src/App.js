4d780bff8eee01cffc3766ce29cb236d5681db0c
CrisTerry <crisdc88@gmail.com>
2020-05-22 21:23:57 -0700

    Add loggin routting to Admin and User

--- sb-client/src/App.js
+++ sb-client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import './App.css';
 import LoginUser from "./pages/userPages/LoginUser"
+import UserDashboard from "./pages/userPages/UserDashboard"
+import AdminDashboard from "./pages/userPages/AdminDashboard"
 
 
 function App() {
@@ -13,9 +15,10 @@ function App() {
        
 
         <Switch>
+      
+        <Route path="/userDashboard" component={UserDashboard}/>
+        <Route path="/adminDashboard" component={AdminDashboard}/>
         <Route exact path="/"  component={LoginUser}/>
-
-
         </Switch>
 
 
---
+++ sb-client/src/pages/userPages/AdminDashboard.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function  AdminDashboard(){
+
+    return (
+        <h1>hello ADMIN!!!!!!!!!!!!!!!!!!!!!!!</h1>
+    )
+}
+
+export default AdminDashboard
